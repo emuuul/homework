d3a0e80be5e3abee3b18f9a169d7137cdf0b81d3
Emil <chelfoto74@yandex.ru>
2019-10-31 11:39:15 -0700

    Initial Commit

---
+++ homework2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework2
+//
+//  Created by user160572 on 10/31/19.
+//  Copyright © 2019 user160572. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
